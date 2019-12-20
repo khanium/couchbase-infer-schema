a7533b8b56fb7da1d1ecff01ca8ab3fb507d2f37
Jose Molina <khanium@hotmail.com>
2019-12-20 17:47:26 +0100

    added infer-schema backend microservice

---
+++ infer-schema/src/main/java/com/couchbase/infer/InferSchemaApp.java
@@ -0,0 +1,13 @@
+package com.couchbase.infer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InferSchemaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InferSchemaApp.class, args);
+    }
+
+}
